26bb6e1948fde7287bf163df4749bd5af724fbeb
MAHESHAIML <maheshs@brightcomgroup.com>
2024-06-10 19:51:22 +0530

    third commit

--- Q_app/apps.py
+++ Q_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class QAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = ' Q_app'
+    name = 'Q_app'
--- run_django.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "D:\Projects\Digital Marketing"  REM Replace with the actual path to your Django project directory
-start "Django Development Server" cmd /k "python manage.py runserver 192.168.30.93:8000"
